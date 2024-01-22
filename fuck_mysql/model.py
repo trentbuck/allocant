a922471b791b22b519f3048d60565b4d75bc7807
Trent W. Buck <trentbuck@gmail.com>
2024-01-22 12:30:58 +1100

    fixup! Add some type annotations
    Do they appear in openapi.json as promised?

--- fuck_mysql/model.py
+++ fuck_mysql/model.py
@@ -7,7 +7,7 @@ class Product(SQLModel, table=True):
     productID: Optional[int] = Field(default=None, primary_key=True)
     productName: Optional[str]
     sellPrice: Optional[int]
-    sellPriceCurrencyTypeID: Annotated[str, "Must always be 'AUD'"], Field(default='AUD')
+    sellPriceCurrencyTypeID: Annotated[str, "Must always be 'AUD'"] = Field(default='AUD')
     sellPriceIncTax: Annotated[bool, "Should always be False for new products."] = Field(default=False)
     description: Optional[str]
     comment: Optional[str]
