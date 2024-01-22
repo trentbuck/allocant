58366a38f9811501535cd5c5a06082ac199f4681
Trent W. Buck <trentbuck@gmail.com>
2024-01-22 12:52:01 +1100

    remove index again
    because IN SQLITE, the index has to EXACTLY match the WHERE clause.
    And right now our test case has
    
        SELECT ... WHERE
            x LIKE :q OR
            y LIKE :q OR
            z LIKE :q
    
    And our index is only of x, so it doesn't help at all, it only hurts.

--- fuck_mysql/model.py
+++ fuck_mysql/model.py
@@ -5,7 +5,7 @@ from sqlmodel import SQLModel, Field
 
 class Product(SQLModel, table=True):
     productID: Optional[int] = Field(default=None, primary_key=True)
-    productName: Optional[str] = Field(index=True)
+    productName: Optional[str]
     sellPrice: Optional[int]
     sellPriceCurrencyTypeID: Annotated[str, "Must always be 'AUD'"] = Field(default='AUD')
     sellPriceIncTax: Annotated[bool, "Should always be False for new products."] = Field(default=False)
