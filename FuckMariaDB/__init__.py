6c5fb89d8beadc72b24a8f5355d1bcd8a4c06fd2
Trent W. Buck <trentbuck@gmail.com>
2024-02-20 10:20:31 +1100

    STFU, python3 -m FuckMariaDB

--- FuckMariaDB/__init__.py
+++ FuckMariaDB/__init__.py
@@ -1,4 +1,7 @@
+from fastapi import FastAPI
+
 __version__ = '0'
+__all__ = ['app']
 __doc__ = """ Proof-of-concept codebase using FastAPI, SQLModel, HTMx, and Handlebars or Jinja2
 
 The point of this is to experiment providing a modern stack accessing an existing old database.
@@ -6,3 +9,6 @@ The old app used MySQL and PHP5, using an in-house app-specific ORM.
 The ORM broke slightly when I upgraded MariaDB 1:10.5.23-0+deb11u1 to 1:10.11.6-0+deb12u1.
 It's too hard to fix, and too hard to downgrade, so
 make new app which provides JUST the interactions that broke (and Ron still needs to do). """
+
+
+app = FastAPI()
---
+++ FuckMariaDB/__main__.py
@@ -0,0 +1,3 @@
+import uvicorn
+from . import app
+uvicorn.run(app, host='0.0.0.0', port=4380)
