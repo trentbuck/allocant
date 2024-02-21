ebc451cb86466b8b898400f3051595e8c3193596
Trent W. Buck <trentbuck@gmail.com>
2024-02-21 13:30:57 +1100

    Workaround https://github.com/tiangolo/fastapi/discussions/10512

--- FuckMariaDB/db.py
+++ FuckMariaDB/db.py
@@ -1,4 +1,5 @@
 from logging import debug
+from pathlib import Path
 from .settings import settings
 from .models import __all__ as models
 
@@ -9,7 +10,16 @@ __all__ = ['engine']
 if settings.in_production:
     engine = create_engine('mysql+mysqldb:///alloc')
 else:
-    engine = create_engine('sqlite:///', echo=True)
+    # FIXME: With sqlite:/// (in-memory), I get this:
+    #        https://github.com/tiangolo/fastapi/discussions/10512
+    #        For now, use a regular file (ugh).
+    for path in Path.cwd().glob('delete-me.db*'):
+        path.unlink()
+    engine = create_engine(
+        'sqlite:///delete-me.db',
+        echo=True,
+        # https://fastapi.tiangolo.com/tutorial/sql-databases/#create-the-sqlalchemy-engine
+        connect_args={'check_same_thread': False})
     # FIXME: this doesn't belong here.
     debug('We must to import %s, as that populates global singleton'
           ' sqlmodel.SQLModel.metadata as a side-effect!',
