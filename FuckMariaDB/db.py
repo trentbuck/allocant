298d162bb19e402fe310c7a81b6f8fbe9eb73936
Trent W. Buck <trentbuck@gmail.com>
2024-02-21 13:29:27 +1100

    CREATE TABLE when not in_production

--- FuckMariaDB/db.py
+++ FuckMariaDB/db.py
@@ -1,6 +1,8 @@
+from logging import debug
 from .settings import settings
+from .models import __all__ as models
 
-from sqlmodel import create_engine
+from sqlmodel import create_engine, SQLModel
 
 __all__ = ['engine']
 
@@ -8,3 +10,8 @@ if settings.in_production:
     engine = create_engine('mysql+mysqldb:///alloc')
 else:
     engine = create_engine('sqlite:///', echo=True)
+    # FIXME: this doesn't belong here.
+    debug('We must to import %s, as that populates global singleton'
+          ' sqlmodel.SQLModel.metadata as a side-effect!',
+          models)
+    SQLModel.metadata.create_all(engine)
