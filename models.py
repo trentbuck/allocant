37d0e27a767d798319251bac31af84df498445eb
Trent W. Buck <trentbuck@gmail.com>
2024-02-26 16:58:30 +1100

    alembic revision --rev 0002 --message 'common_name (not family_name + given_name)' --autogenerate
    INFO  [alembic.runtime.migration] Context impl SQLiteImpl.
    INFO  [alembic.runtime.migration] Will assume non-transactional DDL.
    INFO  [alembic.autogenerate.compare] Detected added column 'person.common_name'
    INFO  [alembic.autogenerate.compare] Detected removed column 'person.family_name'
    INFO  [alembic.autogenerate.compare] Detected removed column 'person.given_names'
      Generating [REDACTED]/alembic/versions/0002_common_name_not_family_name_given_name.py ...  done

--- models.py
+++ models.py
@@ -3,7 +3,4 @@ from sqlmodel import SQLModel, Field
 class Person(SQLModel, table=True):
     """ Test table with deliberately stupid columns """
     person_id: int | None = Field(default=None, primary_key=True)
-    family_name: str
-    # FIXME: can't do this with sqlite:/// without some extra fuckery...
-    # given_names: list[str]
-    given_names: str
+    common_name: str            # Was family_name + given_name
